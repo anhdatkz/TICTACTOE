a36ce73e554b89ff36ca630e93fde949562a6f3e
Your Name <you@example.com>
2022-10-26 07:13:43 +0700

    commit

--- src/component/Board.js
+++ src/component/Board.js
@@ -2,14 +2,15 @@ import React from 'react'
 import Cell from './Cell'
 
 export default function Board(props) {
-    const {cells, onClick, isX} = props
+    const { cells, onClick, isX } = props
     return (
         <div className='board'>
             {cells.map((item, index) => (
-                <Cell key={index} 
-                value={item} 
-                onClick={() => onClick(index)}
-                style={item === "X" ? "is-x" : item === "O" ? "is-o" : ""}></Cell>
+                <Cell key={index}
+                    value={item}
+                    onClick={() => onClick(index)}
+                    style={item === "X" ? "is-x" : item === "O" ? "is-o" : ""}>
+                </Cell>
             ))}
         </div>
     )
